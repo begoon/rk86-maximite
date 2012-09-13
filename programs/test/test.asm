70346795f9a7707091ecc6fc41a20a6718cea5f4
Alexander <alexander@demin.ws>
2012-09-13 22:11:19 +0100

    Added a demo program skeleton.

--- programs/test/test.asm
+++ programs/test/test.asm
@@ -1,8 +1,23 @@
   org 0h
 
-  lxi h, msg
-  call 0f818h
-  jmp 0f86ch
-
-msg: db "priwet", 0dh, 0ah, 0
+  lxi h, 0f800h
+begin:
+  push h
+  lxi d, 76d0h
+  lxi b, 78*30
+loop:
+  mov a, m
+  xra l
+  xra e
+  xra c
+  stax d
+  inx h
+  inx d
+  dcx b
+  mov a, c
+  ora b
+  jnz loop
+  pop h
+  inx h
+  jmp begin
 
